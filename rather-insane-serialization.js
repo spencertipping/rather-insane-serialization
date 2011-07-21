f9d79d5d70f6ad26e0682210ee078b8bfc0915ef
Spencer Tipping <spencer@spencertipping.com>
2011-07-21 12:10:34 -0600

    Designing the new bytecode format.

--- rather-insane-serialization.js
+++ rather-insane-serialization.js
@@ -8,7 +8,7 @@
 // Serializing this array yields this (as a string):
 
 // | (function () {
-//     var _1 = [null];
+//     var _1 = [];
 //     _1[0] = _1;
 //     return _1;
 //   })()
@@ -19,8 +19,8 @@
 // serialized like this:
 
 // | (function () {
-//     var _1 = [null, null];
-//     var _2 = [null];
+//     var _1 = [];
+//     var _2 = [];
 //     _1[0] = _2;
 //     _1[1] = _1;
 //     _2[0] = _2;
@@ -45,7 +45,7 @@ var rather_insane_serialization = {
 
     // Recursive serialization. This does one of two things. For structural types,
     // the values are serialized in terms of literals. For reference types, the
-    // 'refs' table is updated with a new entry and 'null' is emitted as the
+    // 'refs' table is updated with a new entry and a variable is emitted as the
     // serialized output.
     var visit = function (base, index, x) {
       // Handle these two cases immediately. Null and undefined objects will
@@ -57,8 +57,8 @@ var rather_insane_serialization = {
 
       // Handle primitive values. These can't have circular references, since we
       // don't serialize attributes of primitive boxes.
-      if (x.constructor === Number   || x.constructor === Boolean ||
-          x.constructor === Function || x.constructor === RegExp)
+      if (x.constructor === Number || x.constructor === Boolean ||
+          x.constructor === RegExp)
         return x.toString();
 
       if (x.constructor === String)
@@ -74,41 +74,44 @@ var rather_insane_serialization = {
       // it and defer serialization.
       if (x[secret]) {
         // The value has already been visited. In this case we emit an initializer
-        // and return 'null' as the serialization outcome.
-        initializers.push([base, index, x[secret]]);
-        return 'null';
+        // and return its ref as the value.
+        initializers.push([base, index, '_' + x[secret].toString(36)]);
+        return '_' + x[secret].toString(36);
       }
 
-      // Mark the object as having been visited. If we encounter further
-      // references to it, then they will be serialized as null references and
-      // filled in later.
+      // Mark the object as having been visited. We then visit each of the
+      // children; doing so side-effectfully creates initialization statements.
       var x_id = x[secret] = ++id;
       marked.push(x);
 
-      if (x.constructor === Array) {
-        for (var pieces = [], i = 0, l = x.length; i < l; ++i)
-          pieces.push(visit(x_id, i, x[i]));
+      switch (x.constructor) {
+        case Array:
+          for (var pieces = [], i = 0, l = x.length; i < l; ++i)
+            initializers.push([x_id, i, visit(x_id, i, x[i])]);
 
-        refs[x_id] = '[' + pieces.join(',') + ']';
-        if (base) initializers.push([base, index, x_id]);
+          refs[x_id] = '[]';
+          break;
 
-        return 'null';
+        case Function:
+          for (var k in x)
+            if (x.hasOwnProperty(k) && k !== secret)
+              initializers.push([x_id, k, visit(x_id, k, x[k])]);
+
+          visit(x_id, 'prototype', x.prototype);
+          refs[x_id] = x.toString();
+          break;
+
+        default:
+          for (var k in x)
+            if (x.hasOwnProperty(k) && k !== secret)
+              initializers.push([x_id, k, visit(x_id, k, x[k])]);
+
+          refs[x_id] = '{}';
+          break;
       }
 
-      if (x.constructor === Object) {
-        var pieces = [];
-        for (var k in x)
-          if (x.hasOwnProperty(k) && k !== secret)
-            pieces.push((/[^A-Za-z$_]/.test(k) ? visit(null, null, k) : k) + ':' +
-                        visit(x_id, k, x[k]));
-
-        refs[x_id] = '{' + pieces.join(',') + '}';
-        if (base) initializers.push([base, index, x_id]);
-
-        return 'null';
-      }
-
-      throw new Error('crossdomain_rpc.serialize.visit: Invalid object: ' + x);
+      if (base) initializers.push([base, index, '_' + x_id.toString(36)]);
+      return '_' + x_id.toString(36);
     };
 
     // Visit the root value. Because it won't be marked, we don't have to worry
@@ -132,7 +135,7 @@ var rather_insane_serialization = {
       initializer_statements.push(
         '_' + initializers[i][0].toString(36) +
         '[' + visit(null, null, initializers[i][1]) + ']' + '=' +
-        '_' + initializers[i][2].toString(36));
+        initializers[i][2]);
 
     // Generate the closure and surrounding statements.
     var serialization = '(function () {' +
